e0396355788ff5696aea63c28fa73b87e98742e5
Швецов Евгений Валерьевич <evgeniy.shvetcov@fsight.ru>
2018-08-13 13:17:38 +0500

    Concurrency conflicts detection

--- ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Delete.cshtml.cs
@@ -44,14 +44,17 @@ namespace ContosoUniversity.Pages.Instructors
                 return NotFound();
             }
 
-            Instructor = await _context.Instructors.FindAsync(id);
+            Instructor instructor = await _context.Instructors
+                .Include(i => i.CourseAssignments)
+                .SingleAsync(i => i.ID == id);
 
-            if (Instructor != null)
-            {
-                _context.Instructors.Remove(Instructor);
-                await _context.SaveChangesAsync();
-            }
+            var departments = await _context.Departments
+                            .Where(d => d.InstructorID == id)
+                            .ToListAsync();
+            departments.ForEach(d => d.InstructorID = null);
+            _context.Instructors.Remove(instructor);
 
+            await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
     }
