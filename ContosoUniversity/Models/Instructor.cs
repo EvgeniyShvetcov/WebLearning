bc02cae903bc1799a9bb50d6d0f605bcd167a092
Швецов Евгений Валерьевич <evgeniy.shvetcov@fsight.ru>
2018-08-23 11:37:14 +0500

    Database model Inheritance

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -5,22 +5,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor
+    public class Instructor : Person
     {
-        public int ID { get; set; }
-        [Required]
-        [StringLength(50)]
-        [Display(Name = "First Name")]
-        public string LastName { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
         [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime HireDate { get; set; }
-        [Display(Name = "Full Name")]
-        public string FullName{get => LastName + "," + FirstMidName;}
 
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
         public OfficeAssignment OfficeAssignment { get; set; }
