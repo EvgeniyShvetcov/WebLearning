8ff053cb5c1b1206c8ebdac4020efa716ef4048a
Швецов Евгений Валерьевич <evgeniy.shvetcov@fsight.ru>
2018-12-16 19:08:42 +0500

    Add simple reducer and connect it into React component.

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 	render() {
+		const { name, surname, age } = this.props.user;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Мой топ фото</h1>
 				</header>
 				<p className="App-intro">Здесь будут мои самые залайканые фото</p>
+				<p>
+					Меня зовут: {name} {surname}. Мне {age} лет.
+				</p>
 			</div>
 		);
 	}
 }
 
-export default App;
+//Mapping reducers state to React component props
+const mapToStateProps = store => {
+	console.log(store);
+	return {
+		user: store.user,
+	};
+};
+
+export default connect(mapToStateProps)(App);
